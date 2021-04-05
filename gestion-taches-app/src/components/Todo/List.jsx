61d08d4e1d5cb46726c7dc0650d29f36b3194de4
marine <dedola.marine@hotmail.com>
2021-04-05 11:37:27 +0200

    Tri par date de creation

--- gestion-taches-app/src/components/Todo/List.jsx
+++ gestion-taches-app/src/components/Todo/List.jsx
@@ -6,6 +6,13 @@ const List = ({ todoList }) => {
 
     let keyIncr = 0;
 
+    //Tri par date de création la plus récente
+    todoList.sort(function (a, b) {
+        a = new Date(a.createdAt).getTime()
+        b = new Date(b.createdAt).getTime()
+        return b - a;
+    });
+
     //Déclaration de composant Item pour chaque élément de la liste
     return (
         <ul>
